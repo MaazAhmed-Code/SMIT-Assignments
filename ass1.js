af5c50cb0ef091687b6b59ad54ce7847eca4c6f1
MaazAhmed-Code <maazimran112@gmail.com>
2025-02-19 10:14:50 +0500

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,150 @@
+// var username;
+// var myname = "maaz"
+
+// var message = "hello world"
+// alert(message)
+
+// var name = "maaz"
+// var age = "20"
+// var field = "BSCS"
+// var department = "IT"
+// alert(name) alert(age) alert(field) alert(department)
+
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(food)
+
+// var numberofchildren = 2
+// var partnername = "ABC"
+// var location = "USA"
+// var job = "software Developer"
+// alert("You Will Be A "+ job+ "in"+location+"And Married To"+partnername+"with"+numberofchildren+"kids")
+
+// var email = "maazimran112@gmail.com"
+// alert("My Email is "+ email)
+
+// var book = "A smarter way to learn JavaScript"
+// alert("i am tring to learn From a book "+ book)
+
+// document.write("Yay ! i can Write HTML Through javascript")
+
+// var age = 14
+// alert(age)
+
+// var vistied = 21
+// alert("you have visted "+vistied+" times")
+
+// var birthyeaer = 2004
+// document.write("My Birth year is "+ birthyeaer)
+// document.write("Data Type of my variable is " + typeof(birthyeaer))
+
+// var name = prompt()
+// var title = promt()
+// var quantity = +prompt()
+// document.write(name + "Ordered " + quantity + "T shirts On" + title)
+
+// var name, fathername, mothername;
+// var myVariable = "Legal"
+// var $myVariable = "Legal"
+// var _myVariable = "Legal"
+// var variable123 = "Legal"
+// var camelCase = "Legal"
+
+// var 1variable = "Illegal";  // Starts with a number
+// var my-variable = "Illegal"; // Contains a hyphen
+// var var = "Illegal";         // Reserved keyword
+// var my variable = "Illegal"; // Contains a space
+// var my#Variable = "Illegal"; // Contains special character (#)
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain letters, numbers, $ and _. For example: <code>$my_1stVariable</code>.<br>");
+// document.write("Variables must begin with a letter, $ or _. For example: <code>$name, _name or name</code>.<br>");
+// document.write("Variable names are case sensitive.<br>");
+// document.write("Variable names should not be JS reserved keywords.");
+
+// var num1 = 13
+// var num2 = 9
+// var result;
+// alert(num1 + num2)
+// var num1 = 13
+// var num2 = 9
+// var result;
+// alert(num1 - num2)
+// var num1 = 13
+// var num2 = 9
+// var result;
+// alert(num1 / num2)
+// var num1 = 13
+// var num2 = 9
+// var result;
+// alert(num1 * num2)
+
+// var number;
+// document.write("Value after variable declaration is: " + number + "<br>");
+// number = 3;
+// document.write("Initial value: " + number + "<br>");
+// number++;
+// document.write("Value after increment is: " + number + "<br>");
+// number += 7;
+// document.write("Value after addition is: " + number + "<br>");
+// number--;
+// document.write("Value after decrement is: " + number + "<br>");
+// var remainder = number % 3;
+// document.write("The remainder is: " + remainder);
+
+// var ticket = 500
+// document.write("the cost of 5 tickets to a movie is " + ticket*5)
+
+// var num = 4
+// document.write("Multiplication Table oF 4 is")
+// document.write("4 x 1"+ num*1)
+// document.write("4 x 2"+ num*2)
+// document.write("4 x 3"+ num*3)
+// document.write("4 x 4"+ num*4)
+// document.write("4 x 5"+ num*5)
+// document.write("4 x 6"+ num*6)
+// document.write("4 x 7"+ num*7)
+// document.write("4 x 8"+ num*8)
+// document.write("4 x 9"+ num*9)
+// document.write("4 x 10"+ num*10)
+
+// var celsius = 12;
+// var fahrenheitFromCelsius = (celsius * 9) / 5 + 32;
+// document.write(celsius + "°C is " + fahrenheitFromCelsius + "°F<br>");
+// var fahrenheit = 68;
+// var celsiusFromFahrenheit = ((fahrenheit - 32) * 5) / 9;
+// document.write(fahrenheit + "°F is " + celsiusFromFahrenheit + "°C");
+
+// var priceOfItem1 = 500;
+// var priceOfItem2 = 300;
+// var quantityOfItem1 = 2;
+// var quantityOfItem2 = 3;
+// var shippingCharges = 50;
+// var totalCost =
+//   priceOfItem1 * quantityOfItem1 +
+//   priceOfItem2 * quantityOfItem2 +
+//   shippingCharges;
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write("Shipping charges " + shippingCharges + "<br><br>");
+// document.write("Total cost of your order is " + totalCost);
+
+// var totalMarks = 500;
+// var marksObtained = 400;
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%");
+
+// var totalPKR = (10 * 104) + (25 * 28);
+// document.write("<h1>Currency in PKR</h1>");
+// document.write("Total currency in PKR: " + totalPKR);
+
+// var number = 10;
+// var result = ((number + 5) * 10) / 2;
+// document.write("<h1>Arithmetic Operations</h1>");
+// document.write("Initial number: " + number + "<br>");
+// document.write("Result after calculations: " + result);
